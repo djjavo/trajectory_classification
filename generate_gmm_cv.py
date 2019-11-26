7acb048f41a97b5d362aaebede9127e24ae00c6a
James Van Hinsbergh <jvanhins@gmail.com>
2019-11-26 13:11:41 +0000

    Comment out caltrain data

--- generate_gmm_cv.py
+++ generate_gmm_cv.py
@@ -8,26 +8,26 @@ from utils.gmm_cv import build_cross_clust_mixt_cv, compute_classification_score
 print("Caltrain Dataset")
 
 # Download cv_list (for result reproduction)
-output_cv = DATA_DIR + "cv_list_Caltrain.pkl"
-cv_list = pickle.load(open(output_cv, "r"))
+# output_cv = DATA_DIR + "cv_list_Caltrain.pkl"
+# cv_list = pickle.load(open(output_cv, "r"))
 
-# Download data
-data_original = pd.read_pickle(DATA_DIR + "Caltrain.pkl")[["id_traj", "lons", "lats"]].reset_index(drop=True)
-nb_traj = len(data_original.id_traj.unique())
+# # Download data
+# data_original = pd.read_pickle(DATA_DIR + "Caltrain.pkl")[["id_traj", "lons", "lats"]].reset_index(drop=True)
+# nb_traj = len(data_original.id_traj.unique())
 
 
-# Download trajectory's cluster labels
-labels = pickle.load(open(DATA_DIR+"caltrain_traj_labels.pkl", "rb"))
-nb_traj_class = labels.max()+1
+# # Download trajectory's cluster labels
+# labels = pickle.load(open(DATA_DIR+"caltrain_traj_labels.pkl", "rb"))
+# nb_traj_class = labels.max()+1
 
-# Compute gaussian mixture model for all validation dataset and all cluster of trajectory
-print("Build Gaussian Mixture Model for all train dataset under 10 Cross-Validation Procedure.")
+# # Compute gaussian mixture model for all validation dataset and all cluster of trajectory
+# print("Build Gaussian Mixture Model for all train dataset under 10 Cross-Validation Procedure.")
 
-gmm_cv, data_score = build_cross_clust_mixt_cv(data_original, cv_list, nb_traj_class, labels)
-pickle.dump(gmm_cv, open(DATA_DIR + "gmm_cv_models_Caltrain.pkl","wb"))
-data_score.to_pickle(DATA_DIR+"gmm_scores_Caltrain.pkl")
+# gmm_cv, data_score = build_cross_clust_mixt_cv(data_original, cv_list, nb_traj_class, labels)
+# pickle.dump(gmm_cv, open(DATA_DIR + "gmm_cv_models_Caltrain.pkl","wb"))
+# data_score.to_pickle(DATA_DIR+"gmm_scores_Caltrain.pkl")
 
-print("Gaussian Mixture Model build and saved")
+# print("Gaussian Mixture Model build and saved")
 
 
 
