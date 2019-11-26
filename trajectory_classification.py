eab3c5f1a7227f7c7439c6c9a8f2f55b9790a03c
James Van Hinsbergh <jvanhins@gmail.com>
2019-11-26 13:56:50 +0000

    Comment out caltrain data processing

--- trajectory_classification.py
+++ trajectory_classification.py
@@ -4,25 +4,25 @@ from utils.generate_data import DATA_DIR
 from utils.classification import compute_classification_score
 
 
-print("Caltrain Dataset")
+# print("Caltrain Dataset")
 
-# Download data
-data_original = pd.read_pickle(DATA_DIR + "Caltrain.pkl")[["id_traj", "lons", "lats"]].reset_index(drop=True)
-nb_traj = len(data_original.id_traj.unique())
+# # Download data
+# data_original = pd.read_pickle(DATA_DIR + "Caltrain.pkl")[["id_traj", "lons", "lats"]].reset_index(drop=True)
+# nb_traj = len(data_original.id_traj.unique())
 
-#load labels
-labels = pickle.load(open(DATA_DIR+"caltrain_traj_labels.pkl", "rb"))
-
-
-# Load gmm scores
-data_score = pd.read_pickle(DATA_DIR+"gmm_scores_Caltrain.pkl")
+# #load labels
+# labels = pickle.load(open(DATA_DIR+"caltrain_traj_labels.pkl", "rb"))
 
 
-# Compute trajectory's classification score
-pct_good_classification, pct_good_classification_top_3 = compute_classification_score(data_original, data_score, labels, nb_traj)
+# # Load gmm scores
+# data_score = pd.read_pickle(DATA_DIR+"gmm_scores_Caltrain.pkl")
 
-print("Percentage of well classified trajectory : %.2f" %pct_good_classification )
-print("Percentage of of trajectories where good prediction are within best-3 predictions: %.2f \n" %pct_good_classification_top_3 )
+
+# # Compute trajectory's classification score
+# pct_good_classification, pct_good_classification_top_3 = compute_classification_score(data_original, data_score, labels, nb_traj)
+
+# print("Percentage of well classified trajectory : %.2f" %pct_good_classification )
+# print("Percentage of of trajectories where good prediction are within best-3 predictions: %.2f \n" %pct_good_classification_top_3 )
 
 
 ## SaoBento
